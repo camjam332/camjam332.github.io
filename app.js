75c25187bb1f45715598f6027496e80e13d6a04e
camjam332 <cameron.jensen93@gmail.com>
2020-10-30 18:50:36 -0700

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,13 @@ class App{
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0xaaaaaa);
 
-        this.renderer = new THREE.WebGLRenderer({antialias: true});
+        this.renderer = new THREE.WebGLRenderer( {antialias: true} );
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
 
         container.appendChild(this.renderer.domElement);
 
-        this.render.setAnimationLoop(this.render.bind(this));
+        this.renderer.setAnimationLoop(this.render.bind(this));
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
@@ -27,7 +27,7 @@ class App{
     }
     
 	render( ) {   
-        this.render.render(this.scene, this.camera);
+        this.renderer.render(this.scene, this.camera);
     }
 }
 
