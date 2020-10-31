e20ca3791c9e8335e015106aed1df0438d9e784b
camjam332 <cameron.jensen93@gmail.com>
2020-10-30 19:22:42 -0700

    added jsonconfig

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ class App{
 	constructor(){
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
-        
         this.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 0.1, 100);
         this.camera.position.set(0,0,4);
         this.scene = new THREE.Scene();
         this.scene.background = new THREE.Color(0xaaaaaa);
-
         this.renderer = new THREE.WebGLRenderer( {antialias: true} );
         this.renderer.setPixelRatio(window.devicePixelRatio);
         this.renderer.setSize(window.innerWidth, window.innerHeight);
